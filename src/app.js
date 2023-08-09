177303b2933a4f112170cbab6d60bc72d2fc357a
NadiaE-Fernandez <naniifer98@gmail.com>
2023-08-08 23:18:28 -0300

    Modificando

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const app =express();
 const path = require("path");
 
 app.use(express.static("public"));
-
-app.listen(3000, () => "servidor corriendo  en el  puerto 3000");
+const port= process.env.PORT || 3001;
+app.listen(port, () => "servidor corriendo  en el  puerto port ");
 
 app.get("/", (req,res)=> {
     res.sendFile (path.join(__dirname, "./views/home.html"));
